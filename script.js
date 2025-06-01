2e5cab4b2323447b2d380194cae94c49fb89990e
UTSHOB <utshobjaman2012@gmail.com>
2025-06-01 14:56:13 +0600

    add the conditon so that the player is changed perfectly

--- script.js
+++ script.js
@@ -100,17 +100,26 @@
 
 
 
-// selecting  elements 
+//selecting  elements 
 
 const score0El= document.querySelector('#score--0');
 
-const score1El = document.getElementById('score--0');
+const score1El = document.getElementById('score--1');
+
+// const current0El = document.getElementsByName('current--0');
+// const current1El = document.getElementById('current--1')
+
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
+
+
 
 const diceEl =  document.querySelector(".dice");
 
-const buttonNew = document.querySelector(".btn--new")
-const buttonRoll = document.querySelector(".btn--roll")
-const buttonHold = document.querySelector(".btn--hold")
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold')
+
 
 
 
@@ -121,23 +130,26 @@ score1El.textContent =  0 ;
 
 diceEl.classList.add("hidden");
 
-
+let currentScore = 0;
 // rolling dice funtinality 
 
-buttonRoll.addEventListener('click',function(){
-    // creating a random value 
-    const  dice = Math.trunc(Math.random()*6)+1;
-    
-    //display the dice 
-    
-    // diceEl.classList.remove('hidden');
-    diceEl.classList.remove('hidden');
+btnRoll.addEventListener("click",function(){
+    const dice =   Math.trunc(Math.random()*6)+1;
+    console.log(dice);
+//    display dice 
+    diceEl.classList.remove('hidden')
+    diceEl.src =(`dice-${dice}.png`)
 
-    diceEl.src = `dice-${dice}.png`;
-     
 
+    if(dice !== 1 ){
+
+        currentScore += dice;
+        current0El.textContent = currentScore;
+
+1 
+    }else{
+        // sweeth another player 
+
+    }
 })
-
-
-
 
