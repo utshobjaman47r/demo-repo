236df2224edd463cd097022e0c0da436bb557cde
UTSHOB <utshobjaman2012@gmail.com>
2025-06-03 15:19:35 +0600

    adding the funciton that will swith the player

--- script.js
+++ script.js
@@ -129,15 +129,29 @@ const btnHold = document.querySelector('.btn--hold')
 
 
 
+
 score0El.textContent = 0;
 
 score1El.textContent =  0 ;
+let playing = true;
 
 diceEl.classList.add("hidden");
 
 const scores = [0,0]
 let currentScore = 0;
 let activePlayer = 0;
+const switchPlayer = function(){
+     document.getElementById(`current--${activePlayer}`).textContent = 0;
+        currentScore = 0;
+
+         activePlayer = activePlayer === 0 ? 1 : 0; // if activeplayer is === 0 then current player is 1 
+         // or if no (current player is 1) then active player becomes 0 ;
+        //  ? means  if and : means else 
+        
+       
+        player0El.classList.toggle('player--active');
+        player1El.classList.toggle('player--active');
+}
 // rolling dice funtinality 
 
 btnRoll.addEventListener("click",function(){
@@ -158,19 +172,8 @@ btnRoll.addEventListener("click",function(){
 
     }else{
         // sweeth another player 
-        document.getElementById(`current--${activePlayer}`).textContent = 0;
-        currentScore = 0;
-
-         activePlayer = activePlayer === 0 ? 1 : 0; // if activeplayer is === 0 then current player is 1 
-         // or if no (current player is 1) then active player becomes 0 ;
-        //  ? means  if and : means else 
-        
-        // player0El.classList.toggle('player--active');
-        // player1El.classList.toggle('player--acitve'); 
-        // player0El.classList.toggle('player--active');
-        // player1El.classList.toggle('player--active');
-        player0El.classList.toggle('player--active');
-        player1El.classList.toggle('player--active');
+       
+        switchPlayer()
 
 
 
@@ -178,8 +181,24 @@ btnRoll.addEventListener("click",function(){
 })
 
 btnHold.addEventListener('click',function(){
-    
-    scores [activePlayer] += currentScore
 
-    document.getElementById(`current--${activePlayer}`).textContent = scores[activePlayer]
+    console.log("hold button ")
+    
+    scores [activePlayer] += currentScore;
+
+    console.log(scores [activePlayer])
+
+    document.getElementById(`score--${activePlayer}`).textContent = scores[activePlayer];
+
+    if (scores[activePlayer] >= 20) {
+
+        document.querySelector(`.player--${activePlayer}`).classList.add('player--winner');
+        document.querySelector(`.player--${activePlayer}`).classList.remove('player--active');
+        
+    }else{
+           switchPlayer();
+
+    }
+
+ 
 })
